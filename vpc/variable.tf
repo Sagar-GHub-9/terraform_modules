d603a89d037c1f7afc84c8496872f73bbeb25e5f
Sagar Tharkar <tharkarsagar12@gmail.com>
2025-04-12 12:25:08 +0530

    Add files via upload

---
+++ vpc/variable.tf
@@ -0,0 +1,8 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "cidr" {
+    default = "20.0.0.0/16"  
+}
---
+++ vpc/vpc.tf
@@ -0,0 +1,3 @@
+resource "aws_vpc" "batch-27-vpc" {
+  cidr_block = var.cidr
+}
